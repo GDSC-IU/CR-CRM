a81aa815e1c45184da43ef1e980401a8cffe1e3e
bkj281 <66874987+bkj281@users.noreply.github.com>
2021-06-18 17:09:39 +0530

    message schema added

---
+++ server/models/message.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'),
+      msgSchema = new mongoose.Schema({
+        custId: {
+            type: String,
+            required: true
+        },
+        msgDate: { 
+            type: Date, 
+            required: true 
+        }, 
+        msgTitle: { 
+            type: String, 
+            required: true 
+        }, 
+        text: { 
+            type: String, 
+            required: true 
+        }, 
+        status: {
+            type: Boolean 
+        } 
+      });
+
+
+
+module.exports = mongoose.model('message', msgSchema);
