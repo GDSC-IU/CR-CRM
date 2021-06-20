5fd0d564755e9d498d58dbb30c43ac8430f189c1
bkj281 <66874987+bkj281@users.noreply.github.com>
2021-06-21 00:50:47 +0530

    Added Company Employee Registration Forms

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Container, Row, Col, Navbar, Nav} from 'react-bootstrap';
 import '../style/sidebar.css';
 import { Link } from 'react-router-dom';
+import Employee from './empReg';
+import Customer from './custReg';
 
 class Dashboard extends React.Component {
 
---
+++ client/src/components/empReg.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Switch, Route, Link } from "react-router-dom";
+import Login from './login';
+import { Form, Button } from 'react-bootstrap';
+
+class Employee extends Component {
+  
+	render() {
+
+		return (
+			{}
+		);
+	};
+
+}
+
+
+export default Employee;
