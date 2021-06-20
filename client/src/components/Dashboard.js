21116f7d059a06ddd39a24490f9bc5eed7323792
bkj281 <66874987+bkj281@users.noreply.github.com>
2021-06-21 00:39:31 +0530

    Added Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,49 +1,91 @@
 import React from 'react';
-import Sidebar from './sidebar';
-// import { ActivatedRoute } from 'react-router-dom';
+import { Container, Row, Col, Navbar, Nav} from 'react-bootstrap';
+import '../style/sidebar.css';
+import { Link } from 'react-router-dom';
 
 class Dashboard extends React.Component {
 
-    constructor(props) {
-        super (props);
+  constructor(props) {
+      super (props);
+  
+			this.state = {
+          user: '',
+          compID: this.props.match.params.compID
+      }
+  
+	}
 
-        this.state = {
-            user: '',
-            compID: this.props.match.params.compID
-        }
+  componentDidMount() {
+    this.getUser();
+  }
 
+  async getUser() {
+    const { compID } = this.state;
+    try {
+      const res = await fetch(`http://localhost:8080/data-retrieve`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({compID})
+      });
+      const result = await res.json();
+      // console.log(result);
+      this.setState({user: result});
+      console.log(this.state);
     }
+    catch (err) {
+      console.log(err);
+    }    
+  }
 
-    componentDidMount() {
-        this.getUser();
-    }
+  render() {
 
-    async getUser() {
-        const { compID } = this.state;
-        try {
-            const res = await fetch(`http://localhost:8080/data-retrieve`, {
-              method: 'POST',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({compID})
-            });
-            const result = await res.json();
-            // console.log(result);
-            this.setState({user: result});
-            console.log(this.state);
-        }
-        catch (err) {
-            console.log(err);
-        }    
-    }
+		const myStyle = {
+			height: "5rem",
+			width: "100%"
+			
+		};
 
-    render() {
-        return (
-            <>
-                <Sidebar />
-                <h1>Redirected to {this.state.user.compName}</h1>
-            </>
-        );
-    };
+    return (
+      <>
+        <Container fluid>
+          <Row>
+            <Col xs={2} variant="dark" className="bg-primary m-0" id="sidebar-wrapper">
+              <Navbar bg="primary" className="flex-column" variant="dark">
+								<Nav className="flex-column sidebar" variant="dark">
+									<Nav.Item style={{marginBottom: "1rem", textAlign: "center"}}>
+										<Link className="navbar-brand" style={{marginRight: "0px"}} to="/">CR-CRM</Link>
+									</Nav.Item>
+									<Nav.Item>
+										<span className="navbar-brand" style={{marginRight: "0px"}}>{this.state.user.compName}</span>
+									</Nav.Item>
+									<Nav.Item style={{myStyle}}>
+										<Link className="nav-item" style={{color: "white"}} to="/">Add Customer</Link>
+									</Nav.Item>
+									<Nav.Item style={{myStyle}}>
+										<Link className="nav-item" style={{color: "white"}} to="/">Add Employee</Link>
+									</Nav.Item>
+									<Nav.Item style={{myStyle}}>
+										<Link className="nav-item" style={{color: "white"}} to="/">Update Details</Link>
+									</Nav.Item>
+									<Nav.Item style={{myStyle}}>
+										<Link className="nav-item" style={{color: "white"}} to="/">Logout</Link>
+									</Nav.Item>
+									<Nav.Item style={{myStyle}}>
+										<Link className="nav-item" style={{color: "white"}} to="/">Delete Account</Link>
+									</Nav.Item>
+								</Nav>
+							</Navbar>
+            </Col>
+            <Col xs={1}></Col>
+            <Col  xs={9} id="page-content-wrapper">
+              this is a test
+            </Col> 
+          </Row>
+        </Container>
+
+      </>
+    );
+  };
 }
 
 export default Dashboard;
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -13,10 +13,10 @@ class Navigation extends Component {
             		<div className="collapse navbar-collapse">
               		<ul className="navbar-nav ml-auto">
                 		<li className="nav-item">
-                  		<Link className="nav-link" to={"/login"}>Login</Link>
+                  			<Link className="nav-link" to={"/login"}>Login</Link>
                 		</li>
                 		<li className="nav-item">
-                  		<Link className="nav-link" to={"/register"}>Register</Link>
+                  			<Link className="nav-link" to={"/register"}>Register</Link>
                 		</li>
               		</ul>
             		</div>
